62dab817248a8c640821b0ed88e26ac39f826a32
Joren Van Onder <joren@jvo.sh>
2020-04-25 19:20:14 -0700

    Working build

---
+++ webpack.dev.ts
@@ -0,0 +1,29 @@
+import webpack from "webpack";
+import HtmlWebPackPlugin from "html-webpack-plugin";
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./public/index.html",
+});
+
+const config: webpack.Configuration = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
+
+export default config;
