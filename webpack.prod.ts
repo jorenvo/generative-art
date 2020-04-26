ffc8c45c9f270863c78837bda1eb5b04b3579b34
Joren Van Onder <joren@jvo.sh>
2020-04-26 09:58:04 -0700

    Add publicPath for prod builds

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,5 +1,10 @@
 import webPackMerge from "webpack-merge";
 import baseConfig from "./webpack";
 
-const config = webPackMerge(baseConfig, { mode: "production" });
+const config = webPackMerge(baseConfig, {
+  mode: "production",
+  output: {
+    publicPath: "/generative-art/",
+  },
+});
 export default config;
