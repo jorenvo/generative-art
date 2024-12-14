d88ffa400deaa55b3ff30bafa8c9742729e6959c
Joren Van Onder <joren@jvo.sh>
2024-12-14 11:06:47 -0800

    Auto-centering

--- src/ArtPieceSzpakowski.tsx
+++ src/ArtPieceSzpakowski.tsx
@@ -1,4 +1,5 @@
 import { ArtPiece } from "./ArtPiece";
+import { ArtCanvas } from "./App";
 
 // https://www.contemporaryartlibrary.org/project/marian-szpakowski-waclaw-szpakowski-at-galeria-dawid-radziszewski-warsaw-32934/13
 // https://www.contemporaryartlibrary.org/project/circulation-at-galerie-meyer-kainer-vienna-11931/12
@@ -6,17 +7,32 @@ import { ArtPiece } from "./ArtPiece";
 // https://www.contemporaryartlibrary.org/artist/waclaw-szpakowski-16958
 
 class Pen {
-  private ctx: CanvasRenderingContext2D;
+  private ctx: CanvasRenderingContext2D | undefined;
   private orientation: number;
   private col: number;
   private row: number;
+  public minCol: number;
+  public minRow: number;
+  public maxCol: number;
+  public maxRow: number;
 
-  constructor(ctx: CanvasRenderingContext2D, col: number, row: number) {
-    this.ctx = ctx;
+  constructor(
+    col: number = 0,
+    row: number = 0,
+    ctx: CanvasRenderingContext2D | undefined = undefined
+  ) {
     this.orientation = 0;
     this.col = col;
     this.row = row;
-    this.ctx.moveTo(this.col, this.row);
+    this.minCol = Infinity;
+    this.minRow = Infinity;
+    this.maxCol = -Infinity;
+    this.maxRow = -Infinity;
+
+    this.ctx = ctx;
+    if (this.ctx) {
+      this.ctx.moveTo(this.col, this.row);
+    }
   }
 
   private degreesToRadians(degrees: number): number {
@@ -30,7 +46,15 @@ class Pen {
   public move(distance: number) {
     this.col += Math.cos(this.orientation) * distance;
     this.row += Math.sin(this.orientation) * distance;
-    this.ctx.lineTo(this.col, this.row);
+
+    this.minCol = Math.min(this.minCol, this.col);
+    this.minRow = Math.min(this.minRow, this.row);
+    this.maxCol = Math.max(this.maxCol, this.col);
+    this.maxRow = Math.max(this.maxRow, this.row);
+
+    if (this.ctx) {
+      this.ctx.lineTo(this.col, this.row);
+    }
   }
 }
 
@@ -59,12 +83,49 @@ class DrawSequence {
   }
 }
 
+class Drawing {
+  private readonly ctx: CanvasRenderingContext2D;
+  private readonly canvas: ArtCanvas;
+  private readonly sequence: DrawSequence;
+
+  constructor(
+    ctx: CanvasRenderingContext2D,
+    canvas: ArtCanvas,
+    sequence: DrawSequence
+  ) {
+    this.ctx = ctx;
+    this.canvas = canvas;
+    this.sequence = sequence;
+  }
+
+  draw(times: number, spacer: PenFunction = (_: Pen) => {}) {
+    const ghostPen = new Pen();
+    this.sequence.execute(ghostPen, 0, times, spacer);
+    console.log(
+      ghostPen.minCol,
+      ghostPen.maxCol,
+      ghostPen.minRow,
+      ghostPen.maxRow
+    );
+
+    const halfWidth = (ghostPen.maxCol - ghostPen.minCol) / 2;
+    const halfHeight = (ghostPen.maxRow - ghostPen.minRow) / 2;
+
+    const pen = new Pen(
+      this.canvas.draw_width / 2 - halfWidth,
+      this.canvas.draw_height / 2 - halfHeight,
+      this.ctx
+    );
+    this.sequence.execute(pen, 0, times, spacer);
+    this.ctx.stroke();
+  }
+}
+
 export class Szpakowski extends ArtPiece {
   draw() {
     const ctx = this.canvas.getContext2d();
     ctx.beginPath();
 
-    const pen = new Pen(ctx, 10, this.canvas.draw_width * 0.8);
     const largeHorizontal = 50;
     const smallHorizontal = 5;
     const largeVertical = 20;
@@ -108,8 +169,7 @@ export class Szpakowski extends ArtPiece {
       (pen: Pen) => pen.move(largeHorizontal),
     ]);
 
-    sequence.execute(pen, 0, 3, (pen: Pen) => pen.move(smallHorizontal));
-
-    ctx.stroke();
+    const drawing = new Drawing(ctx, this.canvas, sequence);
+    drawing.draw(3, (pen: Pen) => pen.move(smallHorizontal));
   }
 }
