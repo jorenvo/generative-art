4d2fb0b704951018ae1c6aaffc9ecbe19f9d8a7e
Joren Van Onder <joren@jvo.sh>
2026-02-28 14:09:12 -0800

    Publish on github pages

--- webpack_prod.ts
+++ webpack_prod.ts
@@ -1,9 +1,11 @@
+import path from "path";
 import webPackMerge from "webpack-merge";
 import baseConfig from "./webpack";
 
 const config = webPackMerge(baseConfig, {
   mode: "production",
   output: {
+    path: path.resolve(__dirname, "docs"),
     publicPath: "/generative-art/",
   },
 });
